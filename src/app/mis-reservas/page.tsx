6f8eb1df8a3e86bae8144862931020b4c07bef7e
Leonardo <leo.cabral1016@gmail.com>
2023-11-29 15:41:05 -0300

    arreglado mis-reservas

--- src/app/mis-reservas/page.tsx
+++ src/app/mis-reservas/page.tsx
@@ -60,42 +60,44 @@ export default function Servicios() {
         title="Nuestros servicios"
         sub_title="Los mejores viajes de todo el país"
       />
-      <div className="">
-        <SimpleLoader data={servicioUsuario}>
-          <div className="grid grid-cols-2 sm:grid-cols-4 gap-8 p-8 mb-40 w-full">
-            {servicioUsuario.data
-              ?.filter((value) => (value.dni_usuario = user.userSelected.dni))
-              .map((servUsu) => {
-                const puntoIntermedio = puntoIntermedio_data?.find(
-                  (value) =>
-                    value.id_puntoIntermedio == servUsu.id_puntoIntermedio
-                );
-                return (
-                  <CardService
-                    key={generateRandomKey()}
-                    topTitle={`Estado: ${
-                      servUsu.venta ? "Pagado" : "Reservado"
-                    }`}
-                    subTitle={`Costo total: ${servUsu.costo_final}`}
-                    title={
-                      puntoIntermedio?.nombre_ciudad
-                        ? puntoIntermedio?.nombre_ciudad
-                        : ""
-                    }
-                    desc={
-                      servUsu.tipo_atencion !== null
-                        ? `Tipo de atención: ${servUsu.tipo_atencion}`
-                        : "No hay cupos disponibles"
-                    }
-                    onClick={() => {
-                      console.log("Click en reserva");
-                    }}
-                  />
-                );
-              })}
-          </div>
-        </SimpleLoader>
-      </div>
+      {user.isAuthenticated && user.userSelected && (
+        <div className="">
+          <SimpleLoader data={servicioUsuario}>
+            <div className="grid grid-cols-2 sm:grid-cols-4 gap-8 p-8 mb-40 w-full">
+              {servicioUsuario.data
+                ?.filter((value) => (value.dni_usuario = user.userSelected.dni))
+                .map((servUsu) => {
+                  const puntoIntermedio = puntoIntermedio_data?.find(
+                    (value) =>
+                      value.id_puntoIntermedio == servUsu.id_puntoIntermedio
+                  );
+                  return (
+                    <CardService
+                      key={generateRandomKey()}
+                      topTitle={`Estado: ${
+                        servUsu.venta ? "Pagado" : "Reservado"
+                      }`}
+                      subTitle={`Costo total: ${servUsu.costo_final}`}
+                      title={
+                        puntoIntermedio?.nombre_ciudad
+                          ? puntoIntermedio?.nombre_ciudad
+                          : ""
+                      }
+                      desc={
+                        servUsu.tipo_atencion !== null
+                          ? `Tipo de atención: ${servUsu.tipo_atencion}`
+                          : "No hay cupos disponibles"
+                      }
+                      onClick={() => {
+                        console.log("Click en reserva");
+                      }}
+                    />
+                  );
+                })}
+            </div>
+          </SimpleLoader>
+        </div>
+      )}
     </>
   );
 }
